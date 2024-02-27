ac2e5504eb2c69ca2bb9b6c8ca02768af98f6529
Connor Layson <148878769+ConnorLayson@users.noreply.github.com>
2024-02-26 20:05:25 -0500

    Fixed main

--- code/test/main.py
+++ code/test/main.py
@@ -7,6 +7,7 @@ def video():
         yt = YouTube(link)
 
         output = f'Title: {yt.title}/nViews: {yt.views}/nLength: {yt.length}'
-        Element('output').element.innerText = output
+        Element('output').write(output)
     except:
         output = "There was an error with your link"
+        Element('output').write(output)
