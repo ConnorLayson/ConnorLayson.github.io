9273ecfb989a0b7bdf6e3475447685bce7daff39
Connor Layson <148878769+ConnorLayson@users.noreply.github.com>
2024-02-27 09:14:35 -0500

    delete script

--- code/test/main.py
+++
@@ -1,8 +0,0 @@
-from pytube import YouTube
-from pytube import Playlist
-from pyscript import document
-
-def video():
-    link =  document.querySelector("#link-input")
-    output = 'Found video'
-    Element('#output').write(output)
