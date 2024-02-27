5ccf11286a7df24c7c87a36b70f85277774d1e9f
Connor Layson <148878769+ConnorLayson@users.noreply.github.com>
2024-02-26 22:33:55 -0500

    Trying to fix pyscript

--- code/test/main.py
+++ code/test/main.py
@@ -4,11 +4,5 @@ from pyscript import document
 
 def video():
     link =  document.querySelector("#link-input")
-    try:
-        yt = YouTube(link)
-
-        output = 'Found video'
-        Element('#output').write(output)
-    except:
-        output = "There was an error with your link"
-        Element('#output').write(output)
+    output = 'Found video'
+    Element('#output').write(output)
