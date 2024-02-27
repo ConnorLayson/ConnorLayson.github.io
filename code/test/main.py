6f7c2872bb64c29ed138ba687e1f4731881a489c
Connor Layson <148878769+ConnorLayson@users.noreply.github.com>
2024-02-26 21:31:10 -0500

    Update main.py

--- code/test/main.py
+++ code/test/main.py
@@ -1,8 +1,6 @@
 from pytube import YouTube
 from pytube import Playlist
-from pyscript import document 
-
-print("hello")
+from pyscript import document
 
 def video():
     link =  document.querySelector("#link-input")
@@ -10,7 +8,7 @@ def video():
         yt = YouTube(link)
 
         output = 'Found video'
-        Element('output').write(output)
+        Element('#output').write(output)
     except:
         output = "There was an error with your link"
-        Element('output').write(output)
+        Element('#output').write(output)
